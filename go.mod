9c5e9f46c230769ccacd2de36517b2e05c7f8919
jiangchen <jiangchen23@qq.com>
2021-09-20 02:26:09 +0800

    第3周作业 - go并发编程

--- go.mod
+++ go.mod
@@ -3,6 +3,24 @@ module tech.jiangchen/go-advance-homework
 go 1.17
 
 require (
+	github.com/go-kratos/kratos/v2 v2.0.5
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/pkg/errors v0.9.1
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	go.opentelemetry.io/otel v1.0.0-RC2 // indirect
+	go.opentelemetry.io/otel/trace v1.0.0-RC2 // indirect
+	google.golang.org/genproto v0.0.0-20210805201207-89edb61ffb67 // indirect
+	google.golang.org/grpc v1.39.1 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- week03/main.go
+++ week03/main.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
+	g, ctx := errgroup.WithContext(context.Background())
+	httpSrv := http.NewServer(http.Address(":8080"))
+	// http server
+	g.Go(func() error {
+		fmt.Println("http")
+		go func() {
+			<-ctx.Done()
+			fmt.Println("http ctx done")
+			httpSrv.Shutdown(context.TODO())
+		}()
+		return httpSrv.Start(context.Background())
+	})
+
+	// signal
+	g.Go(func() error {
+		// SIGTERM is POSIX specific
+		exitSignals := []os.Signal{os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT}
+		sig := make(chan os.Signal, len(exitSignals))
+		signal.Notify(sig, exitSignals...)
+		for {
+			fmt.Println("signal")
+			select {
+			case <-ctx.Done():
+				fmt.Println("signal ctx done")
+				return ctx.Err()
+			case <-sig:
+				// do something
+				return nil
+			}
+		}
+	})
+
+	// inject error
+	g.Go(func() error {
+		fmt.Println("inject")
+		time.Sleep(10 * time.Second)
+		fmt.Println("inject finish")
+		return errors.New("inject error")
+	})
+
+	err := g.Wait() // first error return
+	fmt.Println(err)
 
 }
